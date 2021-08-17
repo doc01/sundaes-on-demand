c4487b1cb2758d37a2edccd90b196dde4b00b676
DocHub001 <48736430+DocHub001@users.noreply.github.com>
2021-08-17 16:48:25 +0300

    Code and test scoop options images from server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,19 +1,14 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
 
-const server = "http://localhost:3030/";
-
 export const handlers = [
-  // Handles a POST /order request
-  rest.post(`${server}/order`, (req, res, ctx) => {}),
   // Handles a GET /scoops request
-  rest.get(`${server}/scoops`, (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
-        {name: 'Chocolate', imagePath: '/image/chocolate.jpg'}
+        { name: "Chocolate", imagePath: "/image/chocolate.jpg" },
+        { name: "Vanilla", imagePath: "/image/vanilla.jpg" },
       ])
-    )
+    );
   }),
-  // Handles a GET /toppings request
-  rest.get(`${server}/toppings`, (req, res, ctx) => {}),
 ];
