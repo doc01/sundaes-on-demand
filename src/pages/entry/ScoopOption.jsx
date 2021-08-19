f1225bb8c2837b2fd7bf77ee7d1f924ec1f99771
DocHub001 <48736430+DocHub001@users.noreply.github.com>
2021-08-19 14:25:18 +0300

    Error running test in totalUpdates.test

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,6 +1,11 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
-export default function ScoopOption({name, imagePath}) {
+export default function ScoopOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.value);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -8,6 +13,22 @@ export default function ScoopOption({name, imagePath}) {
         alt={`${name} scoop`}
         src={`http://localhost:3030/${imagePath}`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marignTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
---
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import Options from "../Options";
+import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
+
+test("update scoop subtotal when scoops change", async () => {
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+
+  // make sure total start out $0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // update vanila scoops to 1 and check the subtotal
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  console.log(userEvent)
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  // update chocolate scoops to 2 and check subtotal
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  userEvent.clear(chocolateInput);
+  userEvent.clear(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+
+
+});
